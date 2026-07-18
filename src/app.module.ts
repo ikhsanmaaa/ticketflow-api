3405a399bcce5dd532d0100497c21885ea976183
ikhsanmaulana <ikhsanmaaa@gmail.com>
2026-07-18 23:27:25 +0700

    init

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [DashboardModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
